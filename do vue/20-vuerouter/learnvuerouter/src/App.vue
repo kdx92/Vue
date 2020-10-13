26b25923b6b7606e7a2315c47548807971946838
kdx92 <18761925416@163.com>
2020-10-13 23:55:03 +0800

    learn

--- do vue/20-vuerouter/learnvuerouter/src/App.vue
+++ do vue/20-vuerouter/learnvuerouter/src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
-    <router-link to="/index">首页</router-link>
-    <router-link to="/about">关于</router-link>
+    <!-- <router-link to="/index">首页</router-link>
+    <router-link to="/about">关于</router-link> -->
+
+    <button @click="indexClick">index</button>
+    <button @click="aboutClick">about</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    indexClick() {
+      // 通过代码的方式修改路由 vue-router
+      // push=>pushState
+      this.$router.replace('/index')
+      // this.$router.push('/index');
+    },
+    aboutClick() {
+      // this.$router.push('/about');
+      this.$router.replace('/about')
+    }
+    
+  }
 }
 </script>
 
