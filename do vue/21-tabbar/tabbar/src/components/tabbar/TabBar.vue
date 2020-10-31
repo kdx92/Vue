aee620d54e29d6007bf9be6be5f2abf41d8faf9b
kdx92 <18761925416@163.com>
2020-10-31 19:40:02 +0800

    learn

---
+++ do vue/21-tabbar/tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'TabBar'
+}
+</script>
+
+<style scoped>
+  #tab-bar {
+  display: flex;
+  background-color: rgb(244, 244, 244);
+  position: fixed;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  box-shadow: 0px -2px 1px rgba(100, 100, 100, .1);
+  /* x偏移量 y偏移量 模糊程度 */
+}
+
+</style>
---
+++ do vue/21-tabbar/tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <!-- <div id="tab-bar-item">
+
+
+
+    <div id="tab-bar">
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        首页</div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        分类</div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        购物车</div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        我的</div>
+    </div> -->
+
+
+    
+    
+  <div class="tab-bar-item">
+    <slot name="item-icon"></slot>
+    <slot name="item-text"></slot>
+    
+  </div>
+
+</template>
+
+<script>
+  export default {
+  name: 'TabBarItem'
+}
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+</style>
