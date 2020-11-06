03872e3452357bd09df09092bae062d2ad4338a9
kdx92 <18761925416@163.com>
2020-11-06 18:42:48 +0800

    learn

---
+++ do vue/23-Vuex/learn-vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2>{{ message }}</h2>
+    <h2>{{ counter }}</h2>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+
+    <hello-vue :counter="counter"/>
+  </div>
+</template>
+
+<script>
+import HelloVue from './components/HelloVue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloVue
+  },
+  data() {
+    return {
+      message: 'biubiubiu',
+      counter: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
