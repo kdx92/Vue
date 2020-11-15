278a0009dada5170cbad7a93b43f67ffc8c56bd6
kdx92 <18761925416@163.com>
2020-11-15 19:33:26 +0800

    learn

---
+++ do vue/25-project/mall/src/components/common/navbar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+<style scoped>
+  .nav-bar {
+    display: flex;
+    line-height: 44px;
+
+  }
+
+  .left, .right {
+    width: 60px;
+    background-color: #fff;
+  }
+
+  .center {
+    flex: 1;
+    background-color: #fff;
+  }
+</style>
